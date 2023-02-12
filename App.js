33f1b3197b9fd8708352031cffa2fbec522c150e
Kevin <84540535+luminerrr@users.noreply.github.com>
2023-02-12 17:43:50 +0700

    created header for home screen

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
+
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text className="">Ed this</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>    
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 2,
-    backgroundColor: '#f666',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import { View, Text, Image, TextInput, } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {AdjustmentsVerticalIcon, ChevronDownIcon, UserIcon, ServerIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+
+const HomeScreen = () =>{
+  const navigation = useNavigation();
+
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      headerShown:false,
+    })
+  }, [])
+
+  return(
+    <SafeAreaView className='bg-white pt-5'>
+
+        {/* Header */}
+        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+          <Image 
+          source={{
+            uri:'https://links.papareact.com/wru',
+          }}
+          className='h-7 w-7 bg-gray-300 p-4 rounded-full'/>
+
+          <View className="flex-1">
+            <Text className='font-bold text-gray-400 text-xs'>
+              Deliver now
+            </Text>
+            <Text className='font-bold text-xl'>Current Location
+            <ChevronDownIcon size={20} color="#00CCBB"/>
+            </Text>
+          </View>
+          
+          <UserIcon size={35} color="#00CCBB" className='justify-self-end'/>
+        </View>
+
+        {/* Search */}
+        <View>
+          <View className='flex-row space-x-2 bg-gray-200 align-middle'>
+            <MagnifyingGlassIcon color="#00CCBB" size={30}/>
+            <TextInput placeholder="Restaurants and Cuisines"
+            keyboardType="default"
+            />
+          </View>
+
+          <AdjustmentsVerticalIcon color="#00CCBB"/>
+        </View>
+    </SafeAreaView>
+    )
+}
+
+export default HomeScreen
